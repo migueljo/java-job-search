b6e555d82fd66d4148e145d29cf8fbd11b542acd
Miguel <migueljomartinez@gmail.com>
2023-09-14 20:40:58 -0500

    Adding buildAPI method to APIFunction interface

--- src/main/java/com/migueljo/jobsearch/api/APIFunctions.java
+++ src/main/java/com/migueljo/jobsearch/api/APIFunctions.java
@@ -1,5 +1,12 @@
 package com.migueljo.jobsearch.api;
 
+import feign.Feign;
+import feign.gson.GsonDecoder;
+
 public interface APIFunctions {
-	
+	static <T> T buildAPI(Class<T> clazz, String url) {
+		return Feign.builder()
+						.decoder(new GsonDecoder())
+						.target(clazz, url);
+	}
 }
