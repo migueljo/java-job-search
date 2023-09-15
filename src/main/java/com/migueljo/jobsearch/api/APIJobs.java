d3075970489ba5544a9a4dab0a553119f53ff459
Miguel <migueljomartinez@gmail.com>
2023-09-14 20:29:51 -0500

    Adding jobs method to APIJobs interface

--- src/main/java/com/migueljo/jobsearch/api/APIJobs.java
+++ src/main/java/com/migueljo/jobsearch/api/APIJobs.java
@@ -1,5 +1,16 @@
 package com.migueljo.jobsearch.api;
 
+import com.migueljo.jobsearch.JobPosition;
+import feign.Headers;
+import feign.QueryMap;
+import feign.RequestLine;
+
+import java.util.List;
+import java.util.Map;
+
+@Headers("Accept: application/json")
 public interface APIJobs {
-	void jobs();
+
+	@RequestLine("GET /positions.json")
+	List<JobPosition> jobs(@QueryMap Map<String, Object> queryMap);
 }
