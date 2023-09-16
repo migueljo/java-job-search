191c2cd6708ae5df029fd8746d4affa28283b8f5
Miguel <migueljomartinez@gmail.com>
2023-09-15 21:06:30 -0500

    Adding getters and toString

--- src/main/java/com/migueljo/jobsearch/cli/CLIArguments.java
+++ src/main/java/com/migueljo/jobsearch/cli/CLIArguments.java
@@ -11,4 +11,40 @@ public class CLIArguments {
 	private boolean isFullTime = false;
 	private boolean isMarkdown = false;
 	private boolean isHelp = false;
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public boolean isFullTime() {
+		return isFullTime;
+	}
+
+	public boolean isMarkdown() {
+		return isMarkdown;
+	}
+
+	public boolean isHelp() {
+		return isHelp;
+	}
+
+	@Override
+	public String toString() {
+		return "CLIArguments{" +
+						"keyword='" + keyword + '\'' +
+						", location='" + location + '\'' +
+						", page=" + page +
+						", isFullTime=" + isFullTime +
+						", isMarkdown=" + isMarkdown +
+						", isHelp=" + isHelp +
+						'}';
+	}
 }
