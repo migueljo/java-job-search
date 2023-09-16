3cbfc6e3196a7e5497d8a0130d5b8bf66e495914
Miguel <migueljomartinez@gmail.com>
2023-09-15 21:04:09 -0500

    Adding CLIArguments class

---
+++ src/main/java/com/migueljo/jobsearch/cli/CLIArguments.java
@@ -0,0 +1,14 @@
+package com.migueljo.jobsearch.cli;
+
+public class CLIArguments {
+	CLIArguments() {
+
+	}
+
+	private String keyword;
+	private String location;
+	private Integer page = 0;
+	private boolean isFullTime = false;
+	private boolean isMarkdown = false;
+	private boolean isHelp = false;
+}
