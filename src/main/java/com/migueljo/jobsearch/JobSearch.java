e24580263d81261b082aa701420f2faf66d77ee5
Miguel <migueljomartinez@gmail.com>
2023-09-16 23:35:37 -0500

    Creating jCommander object

--- src/main/java/com/migueljo/jobsearch/JobSearch.java
+++ src/main/java/com/migueljo/jobsearch/JobSearch.java
@@ -1,7 +1,13 @@
 package com.migueljo.jobsearch;
 
+import com.beust.jcommander.JCommander;
+import com.migueljo.jobsearch.cli.CLIArguments;
+
+import static com.migueljo.jobsearch.CommanderFunctions.buildCommanderWithName;
+
 public class JobSearch {
 	public static void main(String[] args) {
 		System.out.println("JobSearch.main");
+		JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
 	}
 }
