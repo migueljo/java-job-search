032198164f3908abac4e37db0c63571fd199f4d7
Miguel <migueljomartinez@gmail.com>
2023-09-18 19:34:09 -0500

    Implementing validate method

--- src/main/java/com/migueljo/jobsearch/cli/CLIHelpValidator.java
+++ src/main/java/com/migueljo/jobsearch/cli/CLIHelpValidator.java
@@ -1,4 +1,14 @@
 package com.migueljo.jobsearch.cli;
 
-public class CLIHelpValidator {
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.ParameterException;
+
+public class CLIHelpValidator implements IParameterValidator {
+	@Override
+	public void validate(String name, String value) throws ParameterException {
+		boolean actualValue = Boolean.parseBoolean(value);
+		if (actualValue) {
+			throw new ParameterException("Ayuda solicitada");
+		}
+	}
 }
